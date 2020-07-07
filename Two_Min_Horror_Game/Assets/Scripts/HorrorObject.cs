2023c5c2b3b28b8b2aac60f62c1dd598a3981fb7
afjke36900 <55038688+afjke36900@users.noreply.github.com>
2020-07-07 16:20:02 +0800

    HDRP

---
+++ Two_Min_Horror_Game/Assets/Scripts/HorrorObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class HorrorObject : MonoBehaviour,ITriggerEvent
+{
+    public virtual void TriggerEvent() 
+    {
+        print("!");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "玩家") TriggerEvent();
+    }
+}
