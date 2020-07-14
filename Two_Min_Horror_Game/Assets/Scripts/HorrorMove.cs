a755c4fe5770d888798d8cd70bc9f4b83c80fd06
afjke36900 <55038688+afjke36900@users.noreply.github.com>
2020-07-14 16:30:13 +0800

    更新

---
+++ Two_Min_Horror_Game/Assets/Scripts/HorrorMove.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HorrorMove : HorrorObject
+{
+    [Header("移動物件")]
+    public Transform move;
+    [Header("前往位置")]
+    public Transform target;
+    [Header("移動速度"), Range(1, 100)]
+    public float speed;
+
+    /// <summary>
+    /// 移動物件
+    /// </summary>
+    public override void TriggerEvent()
+    {
+        StartCoroutine(Move());
+    }
+
+    private IEnumerator Move()
+    {
+        // 取得距離(移動，位置)
+        float dis = Vector3.Distance(move.position, target.position);
+
+        // 當距離大於 0.2
+        while (dis >= 0.2f)
+        {
+            // 移動位置前往位置
+            move.position = Vector3.Lerp(move.position, target.position, Time.deltaTime * 0.5f * speed);
+            yield return null;
+        }
+    }
+
+}
--- Two_Min_Horror_Game/Assets/Scripts/HorrorObject.cs
+++ Two_Min_Horror_Game/Assets/Scripts/HorrorObject.cs
@@ -1,10 +1,25 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class HorrorObject : MonoBehaviour,ITriggerEvent
 {
+    /// <summary>
+    /// 剛體
+    /// </summary>
+    private Rigidbody rig;
+
+    private void Awake()
+    {
+        rig = GetComponent<Rigidbody>();
+        //rig.useGravity = false;             // 取消地心引力
+    }
+
+    /// <summary>
+    ///  允許子類別覆寫觸發事件
+    /// </summary>
     public virtual void TriggerEvent() 
     {
-        print("!");
+        
     }
 
     private void OnTriggerEnter(Collider other)
